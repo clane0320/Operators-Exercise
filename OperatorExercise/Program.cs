737c421617749bd8112984acd71c44e8f8c113eb
Chris Lane <clane0320@gmail.com>
2022-12-11 21:12:01 -0600

    excercise 1 and 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Exercise 1
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine("Enter radius of the circle." );
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+            Console.WriteLine(AreaOfCircle(radius));
+
+            
         }
+
+        public static int AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            
+            
+            return (int)area;
+        }
+
     }
 }
